ca6457ac331cd4b950aba377d47a10f50838202a
Thai Duy <chestraxx@gmail.com>
2024-03-17 20:51:50 +0700

    change format validates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
 
   scope :online, -> { where("last_seen_at > ?", 15.minutes.ago) }
 end
