8cc49dd1e43c4c37092190c57a9ae5db64d97dd9
Thai Duy <chestraxx@gmail.com>
2024-03-17 20:52:08 +0700

    add unit test user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without params" do
+    user = User.new
+
+    assert_not user.save, "saved user without email"
+  end
+
+  test "should not save user without email" do
+    user = User.new(
+      email: "",
+      password: "123456"
+    )
+
+    assert_not user.save, "saved user without email"
+  end
+
+  test "should not save user without password" do
+    user = User.new(
+      email: "test@gmail.com",
+      password: ""
+    )
+
+    assert_not user.save, "saved user without password"
+  end
+
+  test "should not save user with email already existed" do
+    assert User.create(
+      email: "test@gmail.com",
+      password: "123456"
+    )
+    assert user = User.new(
+      email: "test@gmail.com",
+      password: "123456"
+    )
+    assert user.invalid?
+    assert user.errors.include?(:email)
+
+    # puts user.errors.full_messages
+  end
 end
