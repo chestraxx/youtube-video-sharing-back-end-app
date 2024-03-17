903767c5e4e2b3f8f129e74b36c295bac41d732a
Thai Duy <chestraxx@gmail.com>
2024-03-18 00:13:36 +0700

    fix must login first when post video to share

--- app/controllers/api/v1/video_controller.rb
+++ app/controllers/api/v1/video_controller.rb
@@ -10,37 +10,43 @@ class Api::V1::VideoController < ActionController::API
   end
 
   def info
-    isUrlValid = VideoInfo.valid_url?(params["video_url"])
-    if !isUrlValid
-      render json: { status: 500}
+    if session[:user_id]
+      isUrlValid = VideoInfo.valid_url?(params["video_url"])
+      if !isUrlValid
+        render json: { status: 500 }
+        return
+      end
+  
+      video = VideoInfo.new(params["video_url"])
+      if !video
+        render json: { status: 500 }
+        return
+      end
+  
+      newVideo = Video.create!(
+        video_id: video.video_id,
+        video_url: params["video_url"],
+        title: video.title,
+        description: video.description,
+        thumbnail: video.thumbnail_large,
+        embed_url: video.embed_url,
+        embed_code: video.embed_code,
+        views_total: video.stats['viewCount'],
+        likes_total: video.stats['likeCount'],
+        user_id: @current_user.id
+      )
+  
+      if !newVideo
+        render json: { status: 500}
+        return
+      end
+  
+      render json: {
+        status: :success,
+        video: newVideo
+      }
+    else
+      render json: { status: 500 }
     end
-
-    video = VideoInfo.new(params["video_url"])
-    if !video
-      render json: { status: 500}
-    end
-
-    newVideo = Video.create!(
-      video_id: video.video_id,
-      video_url: params["video_url"],
-      title: video.title,
-      description: video.description,
-      thumbnail: video.thumbnail_large,
-      embed_url: video.embed_url,
-      embed_code: video.embed_code,
-      views_total: video.stats['viewCount'],
-      likes_total: video.stats['likeCount'],
-      user_id: @current_user.id
-    )
-
-    if !newVideo
-      render json: { status: 500}
-    end
-
-    render json: {
-      status: :success,
-      video: newVideo
-    }
-
   end
 end
