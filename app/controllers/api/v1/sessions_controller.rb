d86ecaf8f1fe5644659307a75e9341b4b5746a2b
Thai Duy <chestraxx@gmail.com>
2024-04-08 21:32:19 +0700

    remove registration controller & test; add registration to login api

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,7 +14,22 @@ class Api::V1::SessionsController < ActionController::API
         user: user
       }
     else
-      render json: { status: 401}
+      user = User.create!(
+        email: params['user']['email'],
+        password: params['user']['password'],
+        password_confirmation: params['user']['password'],
+      )
+
+      if user
+        session[:user_id] = user.id
+        render json: {
+          status: :created,
+          registed: true,
+          user: user
+        }
+      else
+        render json: { status: 500}
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create]
-      resources :registration, only: [:create]
-    
+
       get :logged_in, to: "sessions#logged_in"
       delete :logout, to: "sessions#logout"
 
