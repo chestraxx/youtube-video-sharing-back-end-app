8a1823d6f8f062277ca1caecde800d4397524766
Thai Duy <chestraxx@gmail.com>
2024-04-09 23:38:50 +0700

    add test connection, notification_channel

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,17 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "42"
-  # end
+  test "connects with cookies" do
+    user = User.last
+
+    cookies.signed[:user_id] = user.id
+
+    connect params: { user_id: user.id }
+
+    assert_equal connection.current_user.id, user.id
+  end
+
+  test "connects without cookies" do
+    assert_reject_connection { connect params: { user_id: 12345678 } }
+  end
 end
--- test/channels/notifications_channel_test.rb
+++ test/channels/notifications_channel_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class NotificationsChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
+  test "subscribes" do
+    user = User.last
+
+    stub_connection current_user: user
+
+    subscribe
+    assert subscription.confirmed?
+  end
 end
